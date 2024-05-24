7c50290bd6a3dec7d421af69812c7a9aa79e5af2
Rafa Barber <96293934+R4ph3@users.noreply.github.com>
2024-05-24 23:25:35 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ apt upgrade -y
 
 
 #instalar dependencias necesarias
-apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget libbz2-dev pyinstaller -y
+apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget libbz2-dev -y
 
 #instalar python
 wget https://www.python.org/ftp/python/3.11.0/Python-3.11.0.tgz
